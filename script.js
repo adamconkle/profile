44aa1b6cade02954dbed2b1f61d19c9fb0473fad
Adam <adam.conkle@gmail.com>
2024-11-21 12:37:18 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,1 @@
+<script type="text/javascript" src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js" data-name="bmc-button" data-slug="adamcdesigns" data-color="#FFDD00" data-emoji="☕" data-font="Inter" data-text="Buy me a coffee" data-outline-color="#000000" data-font-color="#000000" data-coffee-color="#ffffff" ></script> 
